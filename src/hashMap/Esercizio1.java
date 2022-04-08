13a21ae5fa8a648a3b9b827434bbd9b573f23634
Mario3999 <mario.malitesta@gmail.com>
2022-04-08 15:21:27 +0200

    Aggiunta HashMap

---
+++ src/hashMap/Esercizio1.java
@@ -0,0 +1,14 @@
+package hashMap;
+
+import java.util.HashMap;
+
+public class Esercizio1 {
+	public static void main(String [] args) {
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		hm.put("Mario Malitesta", 25);
+		hm.put("Marta Giovannini", 30);
+		
+		System.out.println(hm);
+	}
+
+}
---
+++ src/hashMap/Esercizio8.java
@@ -0,0 +1,16 @@
+package hashMap;
+
+import java.util.HashMap;
+
+public class Esercizio8 {
+	public static void main(String [] args) {
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		hm.put("Mario Malitesta", 25);
+		hm.put("Marta Giovannini", 30);
+		
+		
+		
+		System.out.println(hm.containsValue(30));
+	}
+
+}
